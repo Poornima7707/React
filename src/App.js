2ceb8e94ebc97b6396142d77e43dfb0c0eb101a6
Poornima7707 <52906194+Poornima7707@users.noreply.github.com>
2019-11-06 19:06:14 +0530

    done with changes

--- src/App.js
+++ src/App.js
@@ -26,11 +26,17 @@ export default class App extends Component {
       },
     ]
   }
-
-
-  markComplete = (id) => {
-    console.log(id)
-  }
+  
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
 
   delTodo = (id) => {
     this.setState({todos:[...this.state.todos.filter(todo => todo.id !== id)] });
