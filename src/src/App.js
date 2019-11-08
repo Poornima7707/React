d8a25d6d086c5ec170537733ba5ec790135133f0
Poornima7707 <52906194+Poornima7707@users.noreply.github.com>
2019-11-08 16:14:53 +0530

    card design using api
    implemented using axios

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import FetchRandomUser from './FetchRandomUser'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <FetchRandomUser/>
+      </div>
+    )
+  }
+}
